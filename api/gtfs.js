527d14c503d201248f3c0ec4f79a5ed55e9c6b6c
attila137142-coder <attila137142@gmail.com>
2026-04-16 20:08:10 +0200

    Update gtfs.js

--- api/gtfs.js
+++ api/gtfs.js
@@ -12,3 +12,23 @@ export default async function handler(req, res) {
   }
 
 }
+setInterval(render, 30000); // 30 másodpercenként frissít
+function getMinutes(t){
+ const now = new Date();
+ const [h,m] = t.split(":").map(Number);
+ const target = h*60+m;
+ const current = now.getHours()*60+now.getMinutes();
+ return target-current;
+}
+next.forEach(n=>{
+  const min = getMinutes(n.t);
+
+  let cls="white";
+  if(min < 2) cls="red";
+  else if(min > 10) cls="blue";
+
+  html += `
+  <div class="row ${cls}">
+  ${t.line} | ${min} perc múlva | ${t.dest}
+  </div>`;
+});
